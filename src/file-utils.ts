dd6f22eb3a34a7c1eb5dcaf64e16591e97c3ada8
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-03-20 09:52:12 +0900

    refactor: use package

---
+++ src/file-utils.ts
@@ -0,0 +1,34 @@
+import fs from 'fs'
+import path from 'path'
+
+export async function directoryWalk(sourceFolder: string, callback: (path: string, fileName: string) => void) {
+  const files = await fs.promises.readdir(sourceFolder);
+  for (const file of files) {
+    const fPath = path.posix.join(sourceFolder, file)
+    const stats = await fs.promises.stat(fPath)
+
+    if (stats.isDirectory()) {
+      const nextFolder = path.posix.join(sourceFolder, file)
+      directoryWalk(nextFolder, callback)
+    } else {
+      callback(fPath, file)
+    }
+  }
+}
+
+export async function replicateFolderStructure(filePath: string, targetFolder: string) {
+  const outputFolder = path.posix.resolve('./similarity/');
+  const outputPath = filePath.replace(targetFolder, '').split('/').slice(2).join('/');
+  
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
+  }
+
+  outputPath.split('/').slice(-3, -1).reduce((acc, curr) => {
+    const folder = path.posix.join(acc, curr)
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder);
+    }
+    return folder
+  }, outputFolder)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import Tesseract from 'tesseract.js';
+import { directoryWalk, replicateFolderStructure } from './file-utils';
+
 import fs from 'fs'
 import parseArgs from 'minimist'
 import path from 'path'
+import { recognize } from 'chatgpt4pcg';
 
 const outputFolder = path.posix.resolve('./similarity/');
-const dateTimeString = new Date().toISOString().replaceAll(':','_')
+const dateTimeString = new Date().toISOString().replaceAll(':', '_')
 
 async function main() {
   const args = parseArgs(process.argv.slice(2))
@@ -15,69 +17,29 @@ async function main() {
 
   const sourceFolder = argv + '/'
   const sFolder = path.posix.resolve(sourceFolder)
-  await processFolder(sFolder)
+  await directoryWalk(sFolder, processFile)
 }
 
-async function processFolder(sourceFolder: string) {
-  const files = await fs.promises.readdir(sourceFolder);
-  for (const file of files) {
-    const fPath = path.posix.join(sourceFolder, file)
-    const stats = await fs.promises.stat(fPath)
-
-    if (stats.isDirectory()) {
-      const nextFolder = path.posix.join(sourceFolder, file)
-      processFolder(nextFolder)
-    } else {
-      recognize(fPath, file)
-    }
-  }
-}
-
-async function recognize(filePath: string, file: string) {
+async function processFile(filePath: string, file: string) {
   const outputPath = filePath.replace(file, '').split('/').slice(2).join('/');
 
   if (file.indexOf('.png') === -1 && file.indexOf('.jpg') === -1 && file.indexOf('.jpeg') === -1) {
     return
   }
 
-  if (!fs.existsSync(outputFolder)) {
-    fs.mkdirSync(outputFolder);
-  }
+  await replicateFolderStructure(outputPath, outputFolder)
 
-  outputPath.split('/').slice(-3, -1).reduce((acc, curr) => {
-    const folder = path.posix.join(acc, curr)
-    if (!fs.existsSync(folder)) {
-      fs.mkdirSync(folder);
-    }
-    return folder
-  }, outputFolder)
-
-
-  const { data: { text, confidence } } = await Tesseract.recognize(
-    path.posix.resolve(filePath),
-    'eng',
-    { logger: () => { } }
-  )
-
-  const targetCharacter = outputPath.split('/').slice(-2)[0]
+  const { text, confidence } = await recognize(path.posix.resolve(filePath))
 
   const logMessage = `${new Date()} - ${file} - ${text.trim()} - ${confidence}`
   console.log(logMessage)
 
-  const similarityScore = `${file},${score(targetCharacter, text.trim(), confidence)}`
+  const similarityScore = `${file},${confidence}`
   const finalOutputPath = outputPath.split('/').slice(-3, -1).join('/')
 
   await fs.promises.writeFile(path.posix.join(outputFolder, finalOutputPath, file.split('.').slice(0, -1).join('.') + '.txt'), `${text.trim()}\n${confidence}`)
   await fs.promises.appendFile(path.posix.join(outputFolder, finalOutputPath, `_log_${dateTimeString}.txt`), logMessage + '\n')
   await fs.promises.appendFile(path.posix.join(outputFolder, finalOutputPath, `_result_${dateTimeString}.csv`), similarityScore + '\n')
-}
-
-function score(targetCharacter: string, predictedText: string, confidence: number) {
-  if (predictedText === targetCharacter) {
-    return confidence / 100
-  }
-
-  return 0
 }
 
 main()
