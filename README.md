60f884ee7906b6aa6f1b55a6f1a7ae772e4ce36e
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-03-21 04:52:07 +0900

    feat!: change to Python script

--- README.md
+++ README.md
@@ -1,48 +1,50 @@
 # Similarity Checking Script
 
-This repository contains a script for checking the similarity of text in images using Tesseract OCR.
+This repository contains a script for checking the similarity of text in images using a [fine-tuned version of `vit-base-patch16-224-in21k`](https://huggingface.co/pittawat/vit-base-letter) trained on a subset of the [port of CSV version](https://www.kaggle.com/datasets/sachinpatel21/az-handwritten-alphabets-in-csv-format) of [NIST Special Database 19](https://huggingface.co/datasets/pittawat/letter_recognition).
+
+You can try an online demo of the model at [https://huggingface.co/spaces/pittawat/letter_recognizer](https://huggingface.co/spaces/pittawat/letter_recognizer).
 
 ## Installation
 
-To use this script, you must have <a href="https://nodejs.org/en/" target="_new">Node.js</a> and <a href="https://www.npmjs.com/" target="_new">npm</a> installed on your system.
+To use this script, you must have <a href="https://docs.conda.io/en/latest/" target="_new">conda</a> installed on your system.
 
 1. Clone this repository to your local machine.
 2. Navigate to the repository directory in your terminal.
-3. Run `npm install` to install the necessary dependencies.
+3. Create the new conda environment or use an existing one by running `conda create -n chatgpt4pcg python=3.11`. Then activate the environment by running `conda activate chatgpt4pcg`.
+4. Run `pip3 install -r requirements.txt` to install the necessary dependencies.
+5. Run `pip3 install git+https://github.com/huggingface/transformers` to install the latest version of `transformers` from source. This is to prevent an unknown error when importing `AutoImageProcessor` from the package.
 
 ## Usage
 
-1. Run the script using the command `npm start -s="<SOURCE_FOLDER>"`. For example, `npm start -s="./test_images"`.
-2. The script will output the recognized text and its confidence level for each image as a text file in the `similarity/<TARGET_CHARACTER>/<TRIAL_NUMBER>/`, which has the same structure as the source folder.. Files `_log_<DATE_TIME>.txt` and `_result_<DATE_TIME>.csv` will be created inside the same folder.
+1. Changing the variable `SOURCE_FOLDER` to your destination folder. Make sure the folder has the structure as described below.
+2. Run `python3 main.py"` to start the similarity checking process.
+3. The script will output the result in JSON format inside the `similarity` folder under the `SOURCE_FOLDER`. Files `similarity_log_<DATE_TIME>.txt` will be created inside `logs` folder.
 
 Please ensure that the source folder has the following structure:
 
 ```
 <SOURCE_FOLDER>
-├── <TARGET_CHARACTER>
-│   ├── <TRIAL_NUMBER>
-│   │   ├── <FILE_NAME>.jpg
-│   │   ├── <FILE_NAME>.png
-│   │   └── <FILE_NAME>.jpg
-│   ├── <TRIAL_NUMBER>
-│   │   ├── <FILE_NAME>.png
-│   │   ├── <FILE_NAME>.png
-│   │   └── <FILE_NAME>.png
-│   └── <TRIAL_NUMBER>
-│       ├── <FILE_NAME>.png
-│       ├── <FILE_NAME>.png
-│       └── <FILE_NAME>.jpg
-└── <TARGET_CHARACTER>
-    ├── <TRIAL_NUMBER>
-    │   ├── <FILE_NAME>.png
-    │   ├── <FILE_NAME>.png
-    │   └── <FILE_NAME>.png
-    ├── <TRIAL_NUMBER>
-    │   ├── <FILE_NAME>.png
-    │   ├── <FILE_NAME>.jpg
-    │   └── <FILE_NAME>.png
-    └── <TRIAL_NUMBER>
-        ├── <FILE_NAME>.png
-        ├── <FILE_NAME>.jpeg
-        └── <FILE_NAME>.png
+├── <TEAM_NAME>
+|   ├── <STAGE>
+│   │    └── <CHARACTER>
+│   │       ├── <TRIAL_NUMBER>.jpg
+│   │       ├── <TRIAL_NUMBER>.png
+│   │       └── <TRIAL_NUMBER>.jpg
+│   └── <STAGE>
+│        └── <CHARACTER>
+│           ├── <TRIAL_NUMBER>.png
+│           ├── <TRIAL_NUMBER>.png
+│           └── <TRIAL_NUMBER>.png
+└── <TEAM_NAME>
+    ├── <STAGE>
+    │    └── <CHARACTER>
+    │       ├── <TRIAL_NUMBER>.jpg
+    │       ├── <TRIAL_NUMBER>.png
+    │       └── <TRIAL_NUMBER>.jpg
+    └── <STAGE>
+         └── <CHARACTER>
+            ├── <TRIAL_NUMBER>.png
+            ├── <TRIAL_NUMBER>.png
+            └── <TRIAL_NUMBER>.png
+```
 ```
---
+++ main.py
@@ -0,0 +1,141 @@
+import os
+import json
+import string
+from pathlib import Path
+from datetime import datetime
+
+import torch
+from PIL import Image
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+
+processor = AutoImageProcessor.from_pretrained("pittawat/vit-base-letter")
+model = AutoModelForImageClassification.from_pretrained("pittawat/vit-base-letter")
+
+LETTERS_LIST = list(string.ascii_uppercase)
+SOURCE_FOLDER = Path('./competition')
+LOG_FOLDER_NAME = 'logs'
+STAGE = 'images'
+OUTPUT_NAME = 'similarities'
+START_TIME = str(datetime.now().isoformat()).replace(":", "_")
+
+
+def main():
+    log_folder = create_log_folder(SOURCE_FOLDER)
+    teams = list_all_dirs(SOURCE_FOLDER)
+    for team in teams:
+        team_log = f'[{str(datetime.now().isoformat()).replace(":", "_")}] Processing - team: {team}'
+        append_log(log_folder, team_log)
+        path1 = Path(SOURCE_FOLDER, team)
+        characters = list_characters_dirs(path1, STAGE)
+        if characters is not None:
+            for character in characters:
+                character_log = f'[{str(datetime.now().isoformat()).replace(":", "_")}] Processing - team: {team} - character: {character}'
+                append_log(log_folder, character_log)
+                path2 = Path(path1, STAGE, character)
+                trials = list_all_files(path2)
+
+                output = {
+                    'count': len(trials),
+                    'similarities': []
+                }
+
+                similarity_rate = 0
+
+                if trials is not None:
+                    for trial in trials:
+                        trial_log = f'[{str(datetime.now().isoformat()).replace(":", "_")}] Processing - team: {team} - character: {character} - trial: {trial}'
+                        append_log(log_folder, trial_log)
+                        file_path = Path(path2, trial)
+                        raw_result = predict(file_path)
+                        output['similarities'].append(raw_result)
+                        target_prob = search('label', character, raw_result)
+                        similarity_rate += target_prob[0]['softmax_prob']
+
+                output['similarityRate'] = similarity_rate / len(trials)
+                json_output = json.dumps(output, indent=2)
+                output_path = create_output_folder(path2, OUTPUT_NAME, STAGE)
+                output_file_path = Path(output_path, f'{character}.json')
+                with open(output_file_path, 'w') as f:
+                    f.write(json_output)
+
+
+def predict(file):
+    image = Image.open(file)
+    image = image.convert("RGB")
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    softmax_outputs = torch.nn.Softmax(dim=-1)(logits)
+
+    output_list = []
+    for char in LETTERS_LIST:
+        target_prob = softmax_outputs[0][ord(char.upper()) - 65]
+        output_list.append({
+            'label': char,
+            'softmax_prob': target_prob.item()
+        })
+    return output_list
+
+
+def search(key, val, arr):
+    return [el for el in arr if el[key] == val]
+
+
+def list_all_dirs(source_folder):
+    for (_, dirnames, _) in os.walk(source_folder):
+        temp = dirnames
+        out = []
+        for t in temp:
+            path = Path(source_folder, t)
+            if os.path.isdir(path) and not t.startswith('.') and t != LOG_FOLDER_NAME:
+                out.append(t)
+        return out
+
+
+def list_all_files(source_folder):
+    for (_, _, filenames) in os.walk(source_folder):
+        return filenames
+
+
+def list_characters_dirs(source_folder, stage):
+    path = Path(source_folder, stage)
+    characters = list_all_dirs(path)
+    return characters
+
+
+def create_output_folder(path, output_folder_name, stage):
+    root, team, folders = str(path).split('/')[0], str(path).split('/')[1], str(path).split('/')[2:]
+
+    output_dir = Path(root, team, output_folder_name)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    current_dir = Path(output_dir)
+    for folder in folders:
+        if folder == stage:
+            continue
+        current_dir = Path(current_dir, folder)
+        if not os.path.exists(current_dir):
+            os.makedirs(current_dir)
+    return current_dir
+
+
+def create_log_folder(source_folder):
+    output_dir = Path(source_folder, LOG_FOLDER_NAME)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    return output_dir
+
+
+def append_log(log_folder_path, log):
+    print(log)
+    log_file_path = Path(log_folder_path, f'similarity_log_{START_TIME}.txt')
+    if not os.path.exists(log_file_path):
+        with open(log_file_path, 'w') as f:
+            f.write('')
+    with open(log_file_path, 'a') as f:
+        f.write(f'{log}\n')
+
+
+if __name__ == '__main__':
+    main()
